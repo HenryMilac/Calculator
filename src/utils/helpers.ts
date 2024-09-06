e0c2a5e3195ad7c28445ac8f598b61d0d9dad79c
henryramirezdatamatrix <henrymilacprog@gmail.com>
2024-09-06 16:49:13 -0500

    fix: prev: types on utils/helpers

--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -1,17 +1,16 @@
-export const handleClick = (value: string, input: string, setInput: (input: string) => void) => {
-    if (value === ',') {
-        value = '.';
-    }
-    const operators = ['+', '-', '*', '/'];
-    const lastChar = input.slice(-1);
-  
-    if (value === "0" && (input === "" || operators.includes(lastChar))) {
-        return;
-    }
-  
-    setInput((prev) => prev + value);
+export const handleClick = (value: string, input: string, setInput: React.Dispatch<React.SetStateAction<string>>) => {
+  if (value === ',') {
+      value = '.';
+  }
+  const operators = ['+', '-', '*', '/'];
+  const lastChar = input.slice(-1);
+
+  if (value === "0" && (input === "" || operators.includes(lastChar))) {
+      return;
+  }
+
+  setInput((prev: string) => prev + value); // Aquí seguimos usando el callback
 };
-  
 export const handleClear = (setInput: (input: string) => void, setResult: (result: number | null) => void) => {
     setInput("");
     setResult(null);
@@ -26,14 +25,14 @@ export const handleCalculate = (input: string, setResult: (result: number | null
     }
 };
   
-export const handlePlusMinus = (input: string, setInput: (input: string) => void) => {
-    if (input) {
-      setInput((prev) => (parseFloat(prev) * -1).toString());
-    }
+export const handlePlusMinus = (input: string, setInput: React.Dispatch<React.SetStateAction<string>>) => {
+  if (input) {
+      setInput((prev: string) => (parseFloat(prev) * -1).toString());
+  }
 };
-  
-export const handlePercent = (input: string, setInput: (input: string) => void) => {
-    if (input) {
-      setInput((prev) => (parseFloat(prev) / 100).toString());
-    }
-};  
+
+export const handlePercent = (input: string, setInput: React.Dispatch<React.SetStateAction<string>>) => {
+  if (input) {
+      setInput((prev: string) => (parseFloat(prev) / 100).toString());
+  }
+};
